1919e295a598b551e0d0d086ba91e45cb5fe57c2
Citrom333 <citrom333@gmail.com>
2023-08-14 14:36:52 +0200

    Initialize the backend

---
+++ src/main/java/com/example/repairingapp/RepairingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.repairingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repairingapp/RepairingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repairingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
