4eb7c8423284d5e1157359e5cfde21a074eee4fb
Citrom333 <citrom333@gmail.com>
2023-08-18 06:41:01 +0200

    feat: add FixtureService

---
+++ src/main/java/com/example/repairingapp/services/FixtureService.java
@@ -0,0 +1,24 @@
+package com.example.repairingapp.services;
+
+import com.example.repairingapp.entities.Fixture;
+import com.example.repairingapp.repositories.FixtureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class FixtureService {
+    private FixtureRepository fixtureRepository;
+@Autowired
+    public FixtureService(FixtureRepository fixtureRepository) {
+        this.fixtureRepository = fixtureRepository;
+    }
+
+    public void AddNewFixture(Fixture fixture){
+        fixtureRepository.save(fixture);
+    }
+
+    public List<Fixture> GetFixtures() {
+        return fixtureRepository.findAll();
+    }
+}
