feabeb0f44b011d1faf22e1f774c99eae34e76ec
Citrom333 <citrom333@gmail.com>
2023-08-18 06:41:23 +0200

    feat: add VehicleController

---
+++ src/main/java/com/example/repairingapp/controllers/vehicleController.java
@@ -0,0 +1,42 @@
+package com.example.repairingapp.controllers;
+
+import com.example.repairingapp.entities.Vehicle;
+import com.example.repairingapp.services.VehicleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("vehicles")
+public class vehicleController {
+    private final VehicleService vehicleService;
+
+    public vehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public List<Vehicle> getVehicles() {
+        return vehicleService.getVehicles();
+    }
+    @GetMapping("/{id}")
+    public Optional<Vehicle> getVehicle(@PathVariable Long id) {
+        return vehicleService.getVehicle(id);
+    }
+    @PostMapping
+    public Vehicle addNewVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.addNewVehicle(vehicle);
+    }
+
+    @PutMapping("/{id}")
+    public Vehicle updateVehicle(@PathVariable Long id, @RequestBody Vehicle updatedVehicle) {
+        return vehicleService.updateVehicle(id, updatedVehicle);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVehicle(@PathVariable Long id) {
+        vehicleService.deleteVehicle(id);
+    }
+
+}
