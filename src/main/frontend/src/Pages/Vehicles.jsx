1ed689c0a4c84e1c622bd6f3fda153ce3fd2f1c9
Citrom333 <citrom333@gmail.com>
2023-08-29 14:24:45 +0200

    feat: add classNames for css-styling

--- src/main/frontend/src/Pages/Vehicles.jsx
+++ src/main/frontend/src/Pages/Vehicles.jsx
@@ -62,27 +62,29 @@ export default function Vehicles() {
                     <h3>Year of manufacture: {showVehicle.yearOfManufacture}</h3>
                     <h3>Brand: {showVehicle.brand}</h3>
                     <h3>Type: {showVehicle.type}</h3>
-                    <table className='table table-striped'>
-                        <thead>
-                            <tr>
-                                <th>Date of work</th>
-                                <th>Mileage</th>
-                                <th>Name</th>
-                                <th>Comment</th>
-                                <th>Fixtures</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {works.length !== 0 ? works.map((work, i) =>
-                                <tr key={i}>
-                                    <td>{new Date(work.date).getFullYear()}-{(new Date(work.date).getMonth() + 1).toString().padStart(2, '0')}-{new Date(work.date).getDate().toString().padStart(2, '0')}</td>
-                                    <td>{work.mileage}</td>
-                                    <td>{work.name}</td>
-                                    <td>{work.comment}</td>
-                                    <td><div>{work.fixtures.map(f => (<p>{f.name}</p>))}</div></td>
-                                </tr>) : <tr></tr>}
-                        </tbody>
-                    </table>
+                    <div className='table'>
+                        <table>
+                            <thead className='table-head'>
+                                <tr>
+                                    <th>Date of work</th>
+                                    <th>Mileage</th>
+                                    <th>Name</th>
+                                    <th>Comment</th>
+                                    <th>Fixtures</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {works.length !== 0 ? works.map((work, i) =>
+                                    <tr className={`table-row-${i % 2}`} key={i}>
+                                        <td>{new Date(work.date).getFullYear()}-{(new Date(work.date).getMonth() + 1).toString().padStart(2, '0')}-{new Date(work.date).getDate().toString().padStart(2, '0')}</td>
+                                        <td>{work.mileage}</td>
+                                        <td>{work.name}</td>
+                                        <td>{work.comment}</td>
+                                        <td><div>{work.fixtures.map(f => (<p>{f.name}</p>))}</div></td>
+                                    </tr>) : <tr></tr>}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>}
         </div>
     )
