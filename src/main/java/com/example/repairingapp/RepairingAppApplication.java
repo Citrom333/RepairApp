e98de0c94fe3bf6896da4ad1b209204a2479f483
Citrom333 <citrom333@gmail.com>
2023-09-12 09:10:34 +0200

    refactor: remove unused rows from RepairingAppApplication.java

--- src/main/java/com/example/repairingapp/RepairingAppApplication.java
+++ src/main/java/com/example/repairingapp/RepairingAppApplication.java
@@ -1,31 +1,14 @@
 package com.example.repairingapp;
 
-import com.example.repairingapp.entities.Fixture;
-import com.example.repairingapp.entities.Shop;
-import com.example.repairingapp.entities.Vehicle;
-import com.example.repairingapp.repositories.FixtureRepository;
-import com.example.repairingapp.repositories.ShopRepository;
-import com.example.repairingapp.repositories.VehicleRepository;
-import com.example.repairingapp.repositories.WorkRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class RepairingAppApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(RepairingAppApplication.class, args);
 	}
-/*	@Bean
-	CommandLineRunner commandLineRunner(VehicleRepository vehicleRepository, ShopRepository shopRepository, FixtureRepository fixtureRepository, WorkRepository workRepository) {
-		return args -> {
-			vehicleRepository.save(new Vehicle("MNM-260", 2010, "Scania", "460S"));
-			vehicleRepository.save(new Vehicle("FTFT-001", 2022, "Volvo", "FH500"));
-			shopRepository.save(new Shop("Kovács Autóalkatrészek", "Szeged, Kossuth Lajos sgrt. 87, 6724", "info@kovacsauto.hu", "(06 62) 468 444"));
-			shopRepository.save(new Shop("Kamion Shop Kft.", "6791 Szeged, Dorozsmai út 116", "info@kamionshopkft.hu", "+36 20 456 6403"));
-		};
 
-	};*/
 }
