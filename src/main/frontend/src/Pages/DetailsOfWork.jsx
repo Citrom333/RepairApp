3329c1464dbf1b25adec821f33b8f49ebd18e60a
Citrom333 <citrom333@gmail.com>
2023-09-12 09:17:08 +0200

    feat: add new page for details of work

---
+++ src/main/frontend/src/Pages/DetailsOfWork.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import '../App.css'
+export default function DetailsOfWork() {
+    const { id } = useParams();
+    const [work, setWork] = useState();
+    const [message, setMessage] = useState("");
+    const [showConfirm, setShowConfirm] = useState(false);
+    const fetchWork = () =>
+        fetch(`/api/works/${id}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => response.json())
+            .then((json) => {
+                setWork(json);
+                console.log(json)
+            });
+
+
+    const fetchDelete = () => {
+        fetch(`/api/works/${id}`, {
+            method: "DELETE",
+        }).then((response) => {
+            if (response.status === 200) {
+                setMessage("Work deleted");
+                setShowConfirm(false);
+            } else {
+                setMessage("Some error occured");
+            }
+        })
+            .catch((error) => {
+                setMessage("Some error occured");
+            });
+    }
+
+    useEffect(() => {
+        fetchWork();
+        console.log("fetch")
+    }, [])
+
+    const handleDelete = () => {
+        console.log("delete")
+        fetchDelete();
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="page">
+                {work == null ? <p>Loading...</p> : <div>
+                    <h1>{JSON.parse(localStorage.getItem("vehicle")).licensePlate}</h1>
+                    <h3>Date of work: </h3>
+                    <p>{new Date(work.date).getFullYear()}-{(new Date(work.date).getMonth() + 1).toString().padStart(2, '0')}-{new Date(work.date).getDate().toString().padStart(2, '0')}</p>
+                    <h3>Mileage: </h3>
+                    <p>{work.mileage}</p>
+                    <h3>Work: </h3>
+                    <p>{work.name}</p>
+                    <h3>Comment: </h3>
+                    <p>{work.comment}</p>
+                    <h3>Fixtures: </h3>
+                    {work.fixtures.length > 0 ? work.fixtures.map(fixt => <p>{fixt.name}</p>) : <p>No fixture</p>}
+                    <div>
+                        <button onClick={() => setShowConfirm(true)}>Delete</button>
+                    </div>
+                    {showConfirm == false ? "" : <div id="deleteModal" class="modal">
+                        <div class="modal-content">
+                            <p>Do you really want to delete?</p>
+                            <button onClick={() => handleDelete()} id="confirmDelete">Yes</button>
+                            <button onClick={() => setShowConfirm(false)} id="cancelDelete">No</button>
+                        </div>
+                    </div>}
+                    <p>{message}</p>
+                    <div>
+                        <a href="/vehicles">
+                            <button >
+                                Back
+                            </button>
+                        </a>
+                    </div>
+                </div>}
+            </div>
+        </div>
+    )
+}
